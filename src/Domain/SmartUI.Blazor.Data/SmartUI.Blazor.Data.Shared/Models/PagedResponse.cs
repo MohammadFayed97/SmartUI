91a474b01e2f9713a889e68ae5ec306586dcf59f
Mohammad Fayed <mohammadfayed97@gmail.com>
2022-02-05 03:40:03 +0200

    Add Models that work with request and response

---
+++ src/Domain/SmartUI.Blazor.Data/SmartUI.Blazor.Data.Shared/Models/PagedResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace SmartUI.Blazor.Data.Shared;
+
+using AntiRap.Core.Pagination;
+
+public class PagedResponse<TModel> where TModel : class
+{
+    public List<TModel> Items { get; set; }
+    public MetaData? MetaData { get; set; }
+}
