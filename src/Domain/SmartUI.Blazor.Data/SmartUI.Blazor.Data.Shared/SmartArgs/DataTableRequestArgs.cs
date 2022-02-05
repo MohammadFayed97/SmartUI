24d145fcf9803fb6951470e7c7793b7bfb859ba1
Mohammad Fayed <mohammadfayed97@gmail.com>
2022-02-05 03:42:02 +0200

    Add Arguments class that intract with SmartGrid

---
+++ src/Domain/SmartUI.Blazor.Data/SmartUI.Blazor.Data.Shared/SmartArgs/DataTableRequestArgs.cs
@@ -0,0 +1,15 @@
+﻿namespace SmartUI.Blazor.Data.Shared;
+
+public class DataTableRequestArgs<TModel>
+{
+    public FilterQuery filterQuery { get; private set; } = new FilterQuery();
+
+    public DataTableRequestArgs(int pageNumber, int pageSize, List<QueryFilterRule> filterRules)
+    {
+        filterQuery.PageNumber = pageNumber;
+        filterQuery.PageSize = pageSize;
+        filterQuery.FilterRules = filterRules;
+    }
+
+    public DataTableRequestArgs(FilterQuery parameters) => filterQuery = parameters;
+}
