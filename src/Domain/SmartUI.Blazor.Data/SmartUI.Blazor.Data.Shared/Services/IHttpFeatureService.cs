819f86713a3219d9c9256294d1bc9cbae2ae95ed
Mohammad Fayed <mohammadfayed97@gmail.com>
2022-02-05 03:38:38 +0200

    Add HttpService that used to retrive data from an API

---
+++ src/Domain/SmartUI.Blazor.Data/SmartUI.Blazor.Data.Shared/Services/IHttpFeatureService.cs
@@ -0,0 +1,9 @@
+﻿namespace SmartUI.Blazor.Data.Shared;
+
+public interface IHttpFeatureService<TModel>
+        where TModel : class
+{
+    Task<PagedResponse<TModel>> GetAsync(string url, FilterQuery filterQuery = null);
+    Task<IEnumerable<TModel>> GetDataAsync(string url);
+    Task<IEnumerable<TModel>> GetByPredicateAsync(string url, List<QueryFilterRule> queryFilterRules);
+}
