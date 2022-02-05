9b7778f591427618772678b9fe2a46ff6c0ad473
Mohammad Fayed <mohammadfayed97@gmail.com>
2022-02-05 03:45:48 +0200

    Create an Example on SmartGrid Component

--- SmartUI.Sample/Program.cs
+++ SmartUI.Sample/Program.cs
@@ -1,5 +1,8 @@
+using AntiRap.Core.Abstractions;
+using AntiRap.Core.Utilities;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using SmartUI.Blazor.Data.Shared;
 using SmartUI.Sample;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -7,5 +10,6 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
+builder.Services.AddSingleton<IPropertyHandler, PropertyHandler>();
+builder.Services.AddScoped(typeof(IHttpFeatureService<>), typeof(HttpFeatureService<>));
 await builder.Build().RunAsync();
