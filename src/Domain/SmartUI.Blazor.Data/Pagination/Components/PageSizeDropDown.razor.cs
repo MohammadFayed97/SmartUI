3550629ba7dd085893b771ef238d116c108e6134
Mohammad Fayed <mohammadfayed97@gmail.com>
2022-02-09 21:04:20 +0200

    Create Pagiation Component

---
+++ src/Domain/SmartUI.Blazor.Data/Pagination/Components/PageSizeDropDown.razor.cs
@@ -0,0 +1,24 @@
+﻿namespace SmartUI.Blazor.Data;
+
+using SmartUI.Blazor.Shared;
+using Microsoft.AspNetCore.Components;
+
+public partial class PageSizeDropDown : BaseComponent
+{
+    /// <summary>
+    /// It Render DropDownList in the pager which allow us to select PageSize from it 
+    /// </summary>
+    [Parameter]
+    public List<int> PageSizes { get; set; }
+    /// <summary>
+    /// Triggers when selected PageSize from DropDown
+    /// </summary>
+    [Parameter]
+    public EventCallback<int> OnSelectedPageSize { get; set; }
+    [Parameter]
+    public int? Value { get; set; }
+    private async Task OnPageSizeSelected(ChangeEventArgs args)
+    {
+        await OnSelectedPageSize.InvokeAsync(Int32.Parse(args.Value?.ToString()));
+    }
+}
---
+++ src/Domain/SmartUI.Blazor.Data/Pagination/Components/PaginationLink.cs
@@ -0,0 +1,17 @@
+﻿namespace SmartUI.Blazor.Data;
+
+public class PaginationLink
+{
+    public int Page { get; set; }
+    public string Text { get; set; }
+    public bool Enabled { get; set; }
+    public bool Active { get; set; }
+    public bool IsVisible { get; set; }
+
+    public PaginationLink(int page, bool enabled, string text)
+    {
+        Page = page;
+        Enabled = enabled;
+        Text = text;
+    }
+}
